451bbb591634c854c8929dee301122cecd060699
Hashaam Khan <hashaamkhan4247@gmail.com>
2022-12-31 22:05:57 +0500

    added resposive hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Layout/Navbar'
 import './App.css'
+import { MantineProvider } from '@mantine/core'
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-        </Routes>
-        {/* <CustomFooter /> */}
-      </Router>
-    </div>
+    <MantineProvider theme={{ fontFamily: 'Poppins' }}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Hero />} />
+          </Routes>
+        </Router>
+      </div>
+    </MantineProvider>
   )
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,9 +3,13 @@ import {
   Overlay,
   Container,
   Title,
+  Input,
+  MultiSelect,
   Button,
+  Box,
   Text,
 } from '@mantine/core'
+import { IconChevronDown, IconLocation } from '@tabler/icons'
 
 const useStyles = createStyles((theme) => ({
   hero: {
@@ -23,9 +27,10 @@ const useStyles = createStyles((theme) => ({
     paddingBottom: theme.spacing.xl * 6,
     zIndex: 1,
     position: 'relative',
+    // backgroundColor: 'red',
 
     [theme.fn.smallerThan('sm')]: {
-      height: 175,
+      height: 250,
       paddingBottom: theme.spacing.xl * 3,
     },
   },
@@ -33,14 +38,15 @@ const useStyles = createStyles((theme) => ({
   title: {
     color: theme.white,
     fontSize: 60,
-    fontWeight: 900,
-    lineHeight: 1.1,
+    fontWeight: 600,
+    letterSpacing: 2,
     fontFamily: 'poppins',
+    lineHeight: 1.1,
+    textAlign: 'center',
+    marginTop: theme.spacing.xl * 1.5,
 
     [theme.fn.smallerThan('sm')]: {
-      fontSize: 40,
-      lineHeight: 1.2,
-      fontFamily: 'poppins',
+      display: 'none',
     },
 
     [theme.fn.smallerThan('xs')]: {
@@ -52,24 +58,57 @@ const useStyles = createStyles((theme) => ({
 
   description: {
     color: theme.white,
-    maxWidth: 600,
+    marginTop: theme.spacing.md,
+    textAlign: 'center',
+    fontSize: 18,
 
     [theme.fn.smallerThan('sm')]: {
-      maxWidth: '100%',
-      fontSize: theme.fontSizes.sm,
+      display: 'none',
     },
   },
 
   control: {
-    marginTop: theme.spacing.xl * 1.5,
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 20,
+    marginTop: 50,
 
     [theme.fn.smallerThan('sm')]: {
-      width: '100%',
+      flexDirection: 'column',
+      gap: 10,
+      marginTop: 20,
+      width: '80%',
     },
   },
 }))
 
 export default function Hero() {
+  const cityData = [
+    { value: 'islamabad', label: 'Islamabad' },
+    { value: 'rawalpindi', label: 'Rawalpindi' },
+    { value: 'lahore', label: 'Lahore' },
+    { value: 'karachi', label: 'Karachi' },
+    { value: 'peshawar', label: 'Peshawar' },
+    { value: 'quetta', label: 'Quetta' },
+  ]
+  const typeData = [
+    { value: 'house', label: 'House' },
+    { value: 'plot', label: 'Plot' },
+    { value: 'flat', label: 'Flat' },
+    { value: 'shop', label: 'Shop' },
+    { value: 'file', label: 'File' },
+    { value: 'farmhouse', label: 'Farmhouse' },
+    { value: 'building', label: 'Building' },
+  ]
+  const priceData = [
+    { value: '15', label: '<15 Lac' },
+    { value: '30', label: '<30 Lac' },
+    { value: '50', label: '<50 Lac' },
+    { value: '75', label: '<75 Lac' },
+    { value: '1', label: '<1 Crore' },
+    { value: '1', label: '>1 Crore' },
+  ]
+
   const { classes } = useStyles()
 
   return (
@@ -78,9 +117,73 @@ export default function Hero() {
         gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
         opacity={1}
         zIndex={0}
+        blur={3}
       />
       <Container className={classes.container}>
         <Title className={classes.title}>Find Your Dream Home</Title>
+        <Text className={classes.description}>
+          This is where you can find a dream home of your choice without stress
+        </Text>
+        <Container className={classes.control}>
+          <MultiSelect
+            data={cityData}
+            label="Choose City"
+            placeholder="Islamabad"
+            searchable
+            nothingFound="No city found!"
+            size="md"
+            styles={{
+              label: {
+                color: 'white',
+                fontSize: 16,
+              },
+            }}
+          />
+          <MultiSelect
+            data={typeData}
+            label="Choose Type"
+            placeholder="House"
+            searchable
+            nothingFound="No type found!"
+            size="md"
+            styles={{
+              label: {
+                color: 'white',
+                fontSize: 16,
+              },
+            }}
+          />
+          <MultiSelect
+            data={priceData}
+            label="Choose Price Range"
+            placeholder="<15 Lac"
+            size="md"
+            styles={{
+              label: {
+                color: 'white',
+                fontSize: 16,
+              },
+            }}
+          />
+          <Box
+            style={{
+              display: 'flex',
+              alignItems: 'flex-end',
+            }}
+          >
+            <Button
+              variant="filled"
+              fullWidth
+              size="md"
+              style={{
+                backgroundColor: '#D92228',
+                color: 'white',
+              }}
+            >
+              Search
+            </Button>
+          </Box>
+        </Container>
       </Container>
     </div>
   )
