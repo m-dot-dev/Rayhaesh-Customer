6ccb1b6680787fc62b0dc5c70b4ce4807ded1789
Hashaam Khan <hashaamkhan4247@gmail.com>
2023-01-02 13:30:49 +0500

    seperated listings according to their type in home page

--- src/components/Carousel/ListingCarousel.jsx
+++ src/components/Carousel/ListingCarousel.jsx
@@ -1,96 +1,76 @@
 import { Carousel } from '@mantine/carousel'
 import ListingCard from '../Cards/ListingCard'
 import { IconArrowRight, IconArrowLeft } from '@tabler/icons'
-import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Skeleton } from '@mantine/core'
 import { Link } from 'react-router-dom'
 
-export default function ListingCarousel() {
-  const [error, setError] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [allProperties, setAllProperties] = useState([])
-
-  useEffect(() => {
-    axios
-      .get(
-        import.meta.env.VITE_REACT_APP_BACKEND_URL + '/user/getAllProperties',
-      )
-      .then((data) => {
-        setIsLoaded(true)
-        setAllProperties(data.data.body)
-      })
-      .catch((error) => {
-        setIsLoaded(true)
-        setError(error)
-      })
-  }, [])
-
-  if (error) {
-    return <div>Error: {error.message}</div>
-  } else if (!isLoaded) {
+export default function ListingCarousel({ listings, loading, error }) {
+  if (loading) {
     return (
-      <>
-        <Skeleton height={'150px'} />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
-      </>
-    )
-  } else {
-    return (
-      <>
-        {allProperties.length !== 0 && (
-          <Carousel
-            slideSize="25%"
-            slideGap="md"
-            breakpoints={[
-              { maxWidth: 'md', slideSize: '50%' },
-              { maxWidth: 'sm', slideSize: '100%', slideGap: 0 },
-            ]}
-            loop
-            align="center"
-            controlsOffset={0}
-            slidesToScroll={3}
-            nextControlIcon={
-              <IconArrowRight size={18} style={{ color: '#fff' }} />
-            }
-            previousControlIcon={
-              <IconArrowLeft size={18} style={{ color: '#fff' }} />
-            }
-            styles={{
-              control: {
-                border: '1px solid red',
-                width: '40px',
-                height: '40px',
-                backgroundColor: '#D92228',
-                opacity: 100,
-              },
-            }}
-            sx={{
-              maxHeight: '400px',
-            }}
-          >
-            {allProperties.map((property) => (
-              <Carousel.Slide
-                sx={{
-                  '&:hover': {
-                    cursor: 'pointer',
-                  },
-                }}
-              >
-                <Link
-                  to={`/property/${property?._id}`}
-                  state={{ data: property }}
-                  style={{ textDecoration: 'none' }}
-                >
-                  <ListingCard property={property} />
-                </Link>
-              </Carousel.Slide>
-            ))}
-          </Carousel>
-        )}
-      </>
+      <Skeleton
+        style={{ width: '100%', height: '200px' }}
+        variant="rect"
+        radius="md"
+      />
     )
   }
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+
+  return (
+    <>
+      {listings.length !== 0 && (
+        <Carousel
+          slideSize="25%"
+          slideGap="md"
+          breakpoints={[
+            { maxWidth: 'md', slideSize: '50%' },
+            { maxWidth: 'sm', slideSize: '100%', slideGap: 0 },
+          ]}
+          loop
+          align="center"
+          controlsOffset={0}
+          slidesToScroll={3}
+          nextControlIcon={
+            <IconArrowRight size={18} style={{ color: '#fff' }} />
+          }
+          previousControlIcon={
+            <IconArrowLeft size={18} style={{ color: '#fff' }} />
+          }
+          styles={{
+            control: {
+              border: '1px solid red',
+              width: '40px',
+              height: '40px',
+              backgroundColor: '#D92228',
+              opacity: 100,
+            },
+          }}
+          sx={{
+            maxHeight: '400px',
+          }}
+        >
+          {listings.map((property) => (
+            <Carousel.Slide
+              sx={{
+                '&:hover': {
+                  cursor: 'pointer',
+                },
+              }}
+            >
+              <Link
+                to={`/property/${property?._id}`}
+                state={{ data: property }}
+                style={{ textDecoration: 'none' }}
+              >
+                <ListingCard property={property} />
+              </Link>
+            </Carousel.Slide>
+          ))}
+        </Carousel>
+      )}
+    </>
+  )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,6 +6,47 @@ import AgenciesCarousel from '../Carousel/AgenciesCarousel'
 import axios from 'axios'
 
 const Hero = () => {
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(true)
+  const [house, setHouse] = useState([])
+  const [flat, setFlat] = useState([])
+  const [plot, setPlot] = useState([])
+  const [file, setFile] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(
+        import.meta.env.VITE_REACT_APP_BACKEND_URL + '/user/getAllProperties',
+      )
+      .then((data) => {
+        setIsLoaded(false)
+        setHouse(
+          data.data.body.filter(
+            (property) => property.propertySubCategory === 'farmhouse',
+          ),
+        )
+        setFlat(
+          data.data.body.filter(
+            (property) => property.propertySubCategory === 'flat',
+          ),
+        )
+        setPlot(
+          data.data.body.filter(
+            (property) => property.propertySubCategory === 'plot',
+          ),
+        )
+        setFile(
+          data.data.body.filter(
+            (property) => property.propertySubCategory === 'file',
+          ),
+        )
+      })
+      .catch((error) => {
+        setIsLoaded(false)
+        setError(error)
+      })
+  }, [])
+
   const useStyles = createStyles(() => ({
     listingText: {
       fontSize: 30,
@@ -22,21 +63,19 @@ const Hero = () => {
       <Box style={{ marginTop: 50 }}>
         <Box style={{ padding: 20 }}>
           <Text className={classes.listingText}>Premiere House Listings</Text>
-          <ListingCarousel />
+          <ListingCarousel listings={house} loading={isLoaded} error={error} />
         </Box>
         <Box style={{ padding: 20 }}>
           <Text className={classes.listingText}>Hot Flat Listings</Text>
-          <ListingCarousel />
+          <ListingCarousel listings={flat} loading={isLoaded} error={error} />
         </Box>
         <Box style={{ padding: 20 }}>
           <Text className={classes.listingText}>Latest Plot Listings</Text>
-          <ListingCarousel />
+          <ListingCarousel listings={plot} loading={isLoaded} error={error} />
         </Box>
         <Box style={{ padding: 20 }}>
-          <Text className={classes.listingText}>
-            Featured Commercial Listings
-          </Text>
-          <ListingCarousel />
+          <Text className={classes.listingText}>Featured Files</Text>
+          <ListingCarousel listings={file} loading={isLoaded} error={error} />
         </Box>
         <Box style={{ paddingRight: 20, paddingLeft: 20 }}>
           <Text className={classes.listingText}>
